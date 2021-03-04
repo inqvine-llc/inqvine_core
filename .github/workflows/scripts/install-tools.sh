928f9f97ed4d469be259038d3612efd71c574b5e
Ryan Dixon <ryandixon1993@gmail.com>
2021-03-04 19:51:52 +0000

    Update scripts

---
+++ .github/workflows/scripts/install-tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -qq -y
+sudo apt-get install lcov -y
+
+flutter config --no-analytics
+flutter pub global activate melos
+
+echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
+echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
+echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
