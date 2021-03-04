1e9b0e4fe3b796048800cb36c1ac69ffa91260b4
Ryan Dixon <ryandixon1993@gmail.com>
2021-03-04 20:01:01 +0000

    Simplify tools

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -5,7 +5,3 @@ sudo apt-get install lcov -y
 
 flutter config --no-analytics
 flutter pub global activate melos
-
-echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
-echo "/home/cirrus/sdks/flutter/bin/flutter/.pub-cache/bin" >> $GITHUB_PATH
-echo "/home/cirrus/sdks/flutter/bin/flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
