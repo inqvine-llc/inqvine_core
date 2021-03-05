b270fbfa0ce878bafeaa3e939a7ec151b13cd94b
Ryan Dixon <ryandixon1993@gmail.com>
2021-03-05 22:00:19 +0000

    Add more design extensions

--- packages/inqvine_core_main/lib/extensions/inqvine_design_extensions.dart
+++ packages/inqvine_core_main/lib/extensions/inqvine_design_extensions.dart
@@ -3,3 +3,9 @@ import 'package:flutter/material.dart';
 extension InqvineDesignColorExtensions on Color {
   Color get calculateTextForegroundColor => computeLuminance() > 0.5 ? Colors.black : Colors.white;
 }
+
+extension InqvineDesignDoubleExtensions on double {
+  BorderRadius get asBorderRadiusCircular => BorderRadius.circular(this);
+  Widget get asHeightWidth => SizedBox(height: this);
+  Widget get asWidthWidth => SizedBox(height: this);
+}
