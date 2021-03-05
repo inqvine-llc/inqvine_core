cd9ddfe618887f4a68d2330911abf0ca63586600
RegalEye <66254520+RegalEye@users.noreply.github.com>
2021-03-05 22:55:58 +0000

    First pass on commenting extensions

--- packages/inqvine_core_main/README.md
+++ packages/inqvine_core_main/README.md
@@ -43,3 +43,20 @@ A number of built in extensions for the DateTime object built on top of Jiffy.
 *) diffMonthsFromCurrent
 *) diffFromCurrent
 *) toJiffy
+This package contains a variety of basic, commonly used utility extensions (and classes?/functions? etc).
+
+## Contents:
+
+### Context Extensions
+Contains extensions on [BuildContext] for faster tidier use of MediaQueryData functions
+
+### Number Extensions
+Contains extensions on [double] and [int] for converting to and from meters, feet, and inches
+
+### Design Extensions
+Contains extensions on [Color]
+
+### String Extensions
+Contains extensions on [String] for logging events, errors, and general information
+
+### Timestamp Extensions
--- packages/inqvine_core_main/lib/extensions/inqvine_string_extensions.dart
+++ packages/inqvine_core_main/lib/extensions/inqvine_string_extensions.dart
@@ -1,27 +1,38 @@
 import '../inqvine_core_main.dart';
 
 extension InqvineStringExtensions on String {
+  //? A selection of debugging and logging functions, logs are picked up by the InqvineEventBus
+
+  /// Create an information log and pass it to the InqvineEventBus
   void logInfo({
     Map<String, dynamic> metadata = const <String, dynamic>{},
   }) =>
-      _log(InqvineLogEvent(message: this, level: InqvineLogLevel.info, metadata: metadata));
+      _log(InqvineLogEvent(
+          message: this, level: InqvineLogLevel.info, metadata: metadata));
 
+  /// Create a non-fatal error log and pass it to the InqvineEventBus
   void logError({
     Map<String, dynamic> metadata = const <String, dynamic>{},
   }) =>
-      _log(InqvineLogEvent(message: this, level: InqvineLogLevel.error, metadata: metadata));
+      _log(InqvineLogEvent(
+          message: this, level: InqvineLogLevel.error, metadata: metadata));
 
+  /// Create a fatal error log and pass it to the InqvineEventBus
   void logFatal({
     Map<String, dynamic> metadata = const <String, dynamic>{},
   }) =>
-      _log(InqvineLogEvent(message: this, level: InqvineLogLevel.fatal, metadata: metadata));
+      _log(InqvineLogEvent(
+          message: this, level: InqvineLogLevel.fatal, metadata: metadata));
 
+  /// Create a debugging log and pass it to the InqvineEventBus
   void logDebug({
     Map<String, dynamic> metadata = const <String, dynamic>{},
   }) =>
-      _log(InqvineLogEvent(message: this, level: InqvineLogLevel.debug, metadata: metadata));
+      _log(InqvineLogEvent(
+          message: this, level: InqvineLogLevel.debug, metadata: metadata));
 
-  void _log(InqvineLogEvent event) => InqvineServices.instance.publishEvent(event);
+  void _log(InqvineLogEvent event) =>
+      InqvineServices.instance.publishEvent(event);
 
   String get realizeNewLineFeeds {
     return replaceAll(r'\n', '\n');
